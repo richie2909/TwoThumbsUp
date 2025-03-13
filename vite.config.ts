20b8b6f7d074e08fce5b55f3b79bb9cc374c19b3
Richie-00 <itsrichiereal@gmail.com>
2025-03-13 12:52:11 +0800

    hellp

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "TwoThumbsUp",
+  base: "/TwoThumbsUp",
   server: {
     proxy: {
       '/img': {
